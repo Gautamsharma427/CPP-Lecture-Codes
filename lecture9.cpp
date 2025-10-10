7d7700fbaf766c4710382166738ca09c4ef381dd
Gautam <gautamgs20012008@gmail.com>
2025-10-10 09:40:48 +0530

    Lecture 9 Completed

--- lecture9.cpp
+++ lecture9.cpp
@@ -2,6 +2,7 @@
 #include <vector> //or <bits/c++>(but not recommended)
 using namespace std;
 int main(){
+
     // Declaring a integer type vector (Method 1)
     vector<int> vec={90,23,3,4,5,0,3};
     cout<<vec[3];
@@ -22,4 +23,13 @@ int main(){
     cout<<endl<<vec.back();//prints the last element of the vector vec
     cout<<endl<<vec.front();//prints the first element of the vector vec
     vec.pop_back();//would remove the last element of the vector vec
+    cout<<endl<<vec.capacity();//returns the capacity of the vector
+
+    //Problem Number 136 (Leetcode)
+    vector <int> vect = {1,2,1,2,3};
+    int j = 0;
+    for(int i:vect){
+       j = j^i;
+    }
+    cout<<endl<< j;
 }
